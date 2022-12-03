6ad6fdadd6958088a72a89c0a22bed46bd6a2465
longarmin <github4392@arminlanger.com>
2022-12-03 09:25:23 +0100

    changed find_common algo

--- Day3/b.py
+++ Day3/b.py
@@ -1,31 +1,23 @@
-
 def find_common(lines, start, end):
-    import copy
-    for i in range(start, end):
-        common = set()
-        for c in lines[i-1]:
-            if c in lines[i]:
-                common.add(c)
-        lines[i] = copy.deepcopy(common)
-    return(common.pop())
+    for char in lines[start]:
+        if char in lines[start + 1] and char in lines[start + 2]:
+            return calc_prio(char)
+        pass
 
 def calc_prio(c):
-    res = ord(c)-ord('a')
-    if res < 0:
-        res = ord(c) - ord('A') + 26 + 1
+    assert c >= 'A', "Character is out of Range: %s" % c
+    assert c <= 'z', "Character is out of Range: %s" % c
+    if c < 'a':
+        return ord(c) - ord('A') + 27
     else:
-        res += 1
-    return res
+        return ord(c) - ord('a') + 1
 
 def main(inp):
     common = set()
-    lines = []
-    for cnt, line in enumerate(inp):
-        lines.append(line.strip())
+    lines = [line.strip() for line in inp]
     prio_list = []
     for i in range(0, int(len(lines)), 3):
-        a = find_common(lines, i+1, i+3)
-        prio_list.append(calc_prio(a))
+        prio_list.append(find_common(lines, i, i+2))
     print(str(sum(prio_list)))
 
 if __name__=="__main__":
